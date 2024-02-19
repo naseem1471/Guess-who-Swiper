748ff6b851cda796e3d9932f60e7950ad173dc1a
Naseem Akbar <101437439+naseem1471@users.noreply.github.com>
2024-02-19 07:17:21 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -2,42 +2,28 @@ var swiper = new Swiper('.Slider-container', {
   effect: 'cards',
   grabCursor: true,
   centerdSlides: true,
-  loop: false,
+  loop: true,
 });
 // swiper.changeDirection('vertical');
 
-
- // Array to store image URLs
- var images = [];
- const accessKey = "liWsrz3HEPbjJjV2CTZcIPI15i_mEiXtfmrw2f2sRYU"
-
-
- // Function to fetch a new image
-    // Function to fetch a new image
-    async function fetchImage() {
-        const response = await fetch(`https://api.unsplash.com/photos/random?client_id=${accessKey}&query=woman&orientation=portrait&count=1`);
-        const data = await response.json(); // parse the JSON response
-        console.log(data); // log the response
-        const imageUrl = data[0].urls.regular; // get the regular size image URL
-        return imageUrl;
-    }
- 
-  
- // Fetch images for each slide
- async function fetchImages(i) {
-     for (let i = 0; i < swiper.slides.length; i++) {
-         const url = await fetchImage();
-         images.push(url); // Store the image URL in the array
-     }
- }
- 
- 
- fetchImages().then(() => {
-     // Once all images are fetched, update the src attribute of each img element
-     swiper.slides.forEach((slide, index) => {
-         slide.querySelector('img').src = images[index];
-     });
-     swiper.loop = true; // enable loop after images are loaded
-    swiper.update(); // update swiper after changing parameters
-    console.log(images); // log the images array after it's been populated
+// Get the buttons
+const reject = document.getElementById("reject");
+const accept = document.getElementById("accept");
+// Add event listeners to the buttons
+reject.addEventListener("click", function () {
+  // Get the image element of the current slide
+  var img = document.querySelector(".swiper-slide:nth-child(" + (swiper.activeIndex + 1) + ") img");
+  // Set the opacity of the image to 0.3
+  img.style.opacity = 0.05;
+  // Move to the next slide
+  swiper.slideNext();
 });
+accept.addEventListener("click", function () {
+  // Get the image element of the current slide
+  var img = document.querySelector(".swiper-slide:nth-child(" + (swiper.activeIndex + 1) + ") img");
+  // Set the opacity of the image to 1
+  img.style.opacity = 1;
+  // Move to the next slide
+  swiper.slideNext();
+});
+
